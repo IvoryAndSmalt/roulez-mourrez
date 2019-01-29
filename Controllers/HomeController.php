76ede730a030874284076c96c24cb5304c4ea35e
rainbow59 <laura.vangrevenynghe@laposte.net>
2019-01-29 15:28:12 +0100

    Laura -> mvc Twig

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,12 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-// require 'Models/Home.php';
-
-require('env.php');
-
-//Uncomment the section below to connect to database, once env.php has been filled.
-=======
 // Chargement de Twig
 require "LoaderTwig.php";
 
@@ -22,4 +15,3 @@ $template = $twig->load('home.twig');
 
 // On envoi les données a la vue avec Twig
 echo $template->render(array(""));
->>>>>>> c320dc5e05e92ff086e80f0f0c37e98e6954e887
