c320dc5e05e92ff086e80f0f0c37e98e6954e887
jauneloke <jaune.loke@gmail.com>
2019-01-29 08:55:16 +0100

    maj

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,7 +1,17 @@
 <?php
 
+// Chargement de Twig
+require "LoaderTwig.php";
+
+// Chargement du Model
 require('Models/Home.php');
 
+// On charge le fichier voulus du dossier Views 
+$template = $twig->load('home.twig');
 
 
-require('Views/HomeView.php');
+
+
+
+// On envoi les données a la vue avec Twig
+echo $template->render(array(""));
