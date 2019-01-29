80c98640b48dc2620f5b353609307e3105aa7b7c
Lucas <lucas180395@gmail.com>
2019-01-29 15:20:49 +0100

    ajout du sql lucas

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,25 +1,25 @@
 <?php
 
-<<<<<<< HEAD
-// require 'Models/Home.php';
-
-require('env.php');
-
-//Uncomment the section below to connect to database, once env.php has been filled.
-=======
 // Chargement de Twig
-require "LoaderTwig.php";
+// require "LoaderTwig.php";
 
-// Chargement du Model
-require('Models/Home.php');
+// // Chargement du Model
+// require('Models/Home.php');
+$host = 'promo-24.codeur.online';
+$dbname = 'lucasv_roulez-mourrez;charset=utf8';
+$user = 'lucasv';
+$pass = 'vEzn061G+Tzu3A==';
+
+$dbh = new PDO('mysql:host='. $host .';dbname='. $dbname, $user, $pass);
+$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+
+$test = $dbh->query('SELECT * FROM test');
+$test->fetchAll();
+var_dump($test);
 
 // On charge le fichier voulus du dossier Views 
-$template = $twig->load('home.twig');
-
-
-
+// $template = $twig->load('home.twig');
 
 
 // On envoi les données a la vue avec Twig
 echo $template->render(array(""));
->>>>>>> c320dc5e05e92ff086e80f0f0c37e98e6954e887
