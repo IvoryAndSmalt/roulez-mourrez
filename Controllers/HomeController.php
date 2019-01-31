9be3a6dc0a84f973bb14f7f6cd32205db4e15ee5
rainbow59 <laura.vangrevenynghe@laposte.net>
2019-01-31 08:54:19 +0100

    Laura -> toutes les colonnes en php

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -16,17 +16,65 @@ echo "</pre>";
 
 //caracteristiques
 $caracteristiques = getColumns('caracteristiques');
-$colonnes_car = [];
+$colonnes_caracteristiques = [];
 foreach ($caracteristiques as $key => $value) {
-    array_push($colonnes_car, $value['COLUMN_NAME']);
+    array_push($colonnes_caracteristiques, $value['COLUMN_NAME']);
 }
 
-for ($i=0; $i < count($colonnes_car); $i++) {
+for ($i=0; $i < count($colonnes_caracteristiques); $i++) {
     if($i >10 || $i <6){
         continue;
     }
     else{
-        var_dump(getValues('caracteristiques', $colonnes_car[$i]));
+        var_dump(getValues('caracteristiques', $colonnes_caracteristiques[$i]));
+    }
+}
+
+//lieux
+$lieux = getColumns('lieux');
+$colonnes_lieux = [];
+foreach ($lieux as $key => $value) {
+    array_push($colonnes_lieux, $value['COLUMN_NAME']);
+}
+
+for ($i=0; $i < count($colonnes_lieux); $i++) {
+    if($i >10 || $i <6){
+        continue;
+    }
+    else{
+        var_dump(getValues('lieux', $colonnes_lieux[$i]));
+    }
+}
+
+//usagers
+$usagers = getColumns('usagers');
+$colonnes_usagers = [];
+foreach ($usagers as $key => $value) {
+    array_push($colonnes_usagers, $value['COLUMN_NAME']);
+}
+
+for ($i=0; $i < count($colonnes_usagers); $i++) {
+    if($i >10 || $i <6){
+        continue;
+    }
+    else{
+        var_dump(getValues('usagers', $colonnes_usagers[$i]));
+    }
+}
+
+//vehicules
+$vehicules = getColumns('vehicules');
+$colonnes_vehicules = [];
+foreach ($vehicules as $key => $value) {
+    array_push($colonnes_vehicules, $value['COLUMN_NAME']);
+}
+
+for ($i=0; $i < count($colonnes_vehicules); $i++) {
+    if($i >10 || $i <6){
+        continue;
+    }
+    else{
+        var_dump(getValues('vehicules', $colonnes_vehicules[$i]));
     }
 }
 
@@ -35,5 +83,8 @@ $template = $twig->load('home.twig');
 
 // On envoi les données a la vue avec Twig
 echo $template->render(array(
-    "colonnes_car" => $colonnes_car,
+    "colonnes_caracteristiques" => $colonnes_caracteristiques,
+    "colonnes_lieux" => $colonnes_lieux,
+    "colonnes_usagers" => $colonnes_usagers,
+    "colonnes_vehicules" => $colonnes_vehicules,
 ));
