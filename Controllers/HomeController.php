74177f7c897d1fb91f2875ddef60a712a59d14dc
Lucas <lucas180395@gmail.com>
2019-01-31 08:58:12 +0100

    avancement sur les requetes du menu

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -21,6 +21,8 @@ foreach ($caracteristiques as $key => $value) {
     array_push($colonnes_car, $value['COLUMN_NAME']);
 }
 
+
+?><pre><?php
 for ($i=0; $i < count($colonnes_car); $i++) {
     if($i >10 || $i <6){
         continue;
@@ -29,7 +31,7 @@ for ($i=0; $i < count($colonnes_car); $i++) {
         var_dump(getValues('caracteristiques', $colonnes_car[$i]));
     }
 }
-
+?></pre><?php
 // On charge le fichier voulus du dossier Views 
 $template = $twig->load('home.twig');
 
