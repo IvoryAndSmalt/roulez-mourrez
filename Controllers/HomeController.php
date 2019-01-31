01cb4c9086df596c8fe9929c5c9773147cd55328
Lucas <lucas180395@gmail.com>
2019-01-31 11:12:47 +0100

    [supp] nouveau départ

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -6,87 +6,14 @@ require "LoaderTwig.php";
 // // Chargement du Model
 require('Models/Home.php');
 
-$test = testReq("VL seul");
-
-echo "<pre>";
-print_r($test);
-echo "</pre>";
-
-//Charge toutes les options d'intersection
-
-//caracteristiques
-$caracteristiques = getColumns('caracteristiques');
-$colonnes_car = [];
-foreach ($caracteristiques as $key => $value) {
-    array_push($colonnes_car, $value['COLUMN_NAME']);
-}
+$carac1 = getValues('caracteristiques', 'lumiere');
+var_dump($carac1);
 
 
-?><pre><?php
-for ($i=0; $i < count($colonnes_car); $i++) {
-    if($i >10 || $i <6){
-        continue;
-    }
-    else{
-        // var_dump(getValues('caracteristiques', $colonnes_car[$i]));
-    }
-}
-
-//lieux
-$lieux = getColumns('lieux');
-$colonnes_lieux = [];
-foreach ($lieux as $key => $value) {
-    array_push($colonnes_lieux, $value['COLUMN_NAME']);
-}
-
-for ($i=0; $i < count($colonnes_lieux); $i++) {
-    if($i >10 || $i <6){
-        continue;
-    }
-    else{
-        // var_dump(getValues('lieux', $colonnes_lieux[$i]));
-    }
-}
-
-//usagers
-$usagers = getColumns('usagers');
-$colonnes_usagers = [];
-foreach ($usagers as $key => $value) {
-    array_push($colonnes_usagers, $value['COLUMN_NAME']);
-}
-
-for ($i=0; $i < count($colonnes_usagers); $i++) {
-    if($i >10 || $i <6){
-        continue;
-    }
-    else{
-        // var_dump(getValues('usagers', $colonnes_usagers[$i]));
-    }
-}
-
-//vehicules
-$vehicules = getColumns('vehicules');
-$colonnes_vehicules = [];
-foreach ($vehicules as $key => $value) {
-    array_push($colonnes_vehicules, $value['COLUMN_NAME']);
-}
-
-for ($i=0; $i < count($colonnes_vehicules); $i++) {
-    if($i >10 || $i <6){
-        continue;
-    }
-    else{
-        // var_dump(getValues('vehicules', $colonnes_vehicules[$i]));
-    }
-}
-?></pre><?php
 // On charge le fichier voulus du dossier Views 
 $template = $twig->load('home.twig');
 
 // On envoi les données a la vue avec Twig
 echo $template->render(array(
-    "colonnes_car" => $colonnes_car,
-    "colonnes_lieux" => $colonnes_lieux,
-    "colonnes_usagers" => $colonnes_usagers,
-    "colonnes_vehicules" => $colonnes_vehicules,
+    "colonnes_car" => $colonnes_car
 ));
--- Models/Home.php
+++ Models/Home.php
@@ -6,55 +6,13 @@ require('env.php');
 $dbh = new PDO('mysql:host='. $host .';dbname='. $dbname, $user, $pass);
 $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 
-//Function returning one column from a given ID.
-function testReq($VL){
-    global $dbh;
-
-    $one_col = $dbh->prepare('SELECT COUNT(*) as co FROM vehicules WHERE categorie_v=?');
-    $one_col->execute([$VL]);
-
-    $one_col = $one_col->fetchAll();
-
-    return $one_col;
-};
-
-//cas quand il sélectionne 2 trucs
-function retourne2($choix1, $choix2){
-    $one_col = $dbh->prepare('SELECT $table.choix2.value, $choix2 FROM $table1, $table2 WHERE ofzkof=? AND ozeezfoz=?');
-    $one_col->execute([$choix1, $choix2]);
-
-    $one_col = $one_col->fetchAll();
-    $array = [];
-    foreach ($onecol as $key => $value) {
-        array_push($array, $value['numa']);
-        $min = min($array);
-        $max = max($array);
-    }
-    return $table = [$min, $max];
-}
-
-// function retourne3($table){
-//     $one_col = $dbh->prepare('SELECT $table.choix2.value, $choix2 FROM $table1, $table2 WHERE numa <= $table[0] AND numa >= $table[1]');
-// }
-
-function getColumns($table){
-    global $dbh;
-
-    $colonnes = $dbh->prepare('SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ?');
-
-    $colonnes->execute([$table]);
-
-    return $colonnes->fetchAll();
-}
-
 function getValues($table, $colonne){
     global $dbh;
 
-    $one_col = $dbh->prepare('SELECT '.$colonne.' FROM '.$table.' GROUP BY '.$colonne);
-    $one_col->execute();
+    $valeur = $dbh->prepare('SELECT '.$colonne.' FROM '.$table.' GROUP BY '.$colonne);
+    $valeur->execute();
 
-    $one_col = $one_col->fetchAll();
+    $valeur = $valeur->fetchAll(PDO::FETCH_ASSOC);
 
-    return $one_col;
+    return $valeur;
 }
-
