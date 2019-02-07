ddf891353759092158e1cfc79a5a7d4863e134f9
Lucas <lucas180395@gmail.com>
2019-02-07 08:36:47 +0100

    requête en ajax coreect pour le total

--- Controllers/JajaxController.php
+++ Controllers/JajaxController.php
@@ -1,6 +1,6 @@
 <?php
 
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 
 $host = "localhost";
 $dbname = "roulez-mourrez;charset=utf8";
@@ -12,12 +12,9 @@ $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
 
 function getValues($table, $colonne){
     global $dbh;
-
     $valeur = $dbh->prepare('SELECT '.$colonne.' FROM '.$table.' GROUP BY '.$colonne);
     $valeur->execute();
-
     $valeur = $valeur->fetchAll(PDO::FETCH_ASSOC);
-
     return $valeur;
 }
 
@@ -59,23 +56,11 @@ function getRes($arg1 = "vehicules_categorie_v@%", $arg2 = "vehicules_categorie_
     '.$table4.'.'.$colonne4.' LIKE ? AND
     '.$table5.'.'.$colonne5.' LIKE ?;';
 
-    // echo $sql."<br>";
-    // echo "<br>Recherche avec :<br>";
-    // echo $table1.".".$colonne1."=>".$valeur1."<br>";
-    // echo $table2.".".$colonne2."=>".$valeur2."<br>";
-    // echo $table3.".".$colonne3."=>".$valeur3."<br>";
-    // echo $table4.".".$colonne4."=>".$valeur4."<br>";
-    // echo $table5.".".$colonne5."=>".$valeur5."<br>";
-
     $valeur = $dbh->prepare($sql);
-
     $valeur->execute([$valeur1, $valeur2, $valeur3, $valeur4, $valeur5]);
-
     $valeur = $valeur->fetchAll(PDO::FETCH_ASSOC); 
-
     return $valeur;
 }
-
 
 function getDepartements($arg1 = "vehicules_categorie_v@%", $arg2 = "vehicules_categorie_v@%", $arg3 = "vehicules_categorie_v@%", $arg4 = "vehicules_categorie_v@%", $arg5 = "vehicules_categorie_v@%"){
     global $dbh;
@@ -108,83 +93,69 @@ function getDepartements($arg1 = "vehicules_categorie_v@%", $arg2 = "vehicules_c
     lieux.numa = usagers.numa AND 
     lieux.numa = vehicules.numa AND 
     usagers.numa = vehicules.numa AND
-
-
     '.$table1.'.'.$colonne1.' LIKE ? AND
     '.$table2.'.'.$colonne2.' LIKE ? AND
     '.$table3.'.'.$colonne3.' LIKE ? AND
     '.$table4.'.'.$colonne4.' LIKE ? AND
     '.$table5.'.'.$colonne5.' LIKE ?
-    
     GROUP BY caracteristiques.departement;';
 
-    // echo $sql."<br>";
-    // echo "<br>Recherche avec :<br>";
-    // echo $table1.".".$colonne1."=>".$valeur1."<br>";
-    // echo $table2.".".$colonne2."=>".$valeur2."<br>";
-    // echo $table3.".".$colonne3."=>".$valeur3."<br>";
-    // echo $table4.".".$colonne4."=>".$valeur4."<br>";
-    // echo $table5.".".$colonne5."=>".$valeur5."<br>";
-
     $valeur = $dbh->prepare($sql);
-
     $valeur->execute([$valeur1, $valeur2, $valeur3, $valeur4, $valeur5]);
-
     $valeur = $valeur->fetchAll(PDO::FETCH_ASSOC); 
-
     return $valeur;
 }
 
-
-
 if(isset($_POST) && !empty($_POST)){
     $i = 1;
-    // var_dump($_POST);
 
-    foreach ($_POST as $key => $value) {
-        if($value !== ""){
-            $arg = "arg".$i;
-            $$arg = $key. "@". $value;
-            $i++;
-        }
-    }
-    
-    // appeler la fonction avec les arguments donnés
-    if(isset($arg5)){
-        $getres = getRes($arg1, $arg2, $arg3, $arg4, $arg5);
-        $resultat = $getres[0]['total'];
-    }
-    elseif (isset($arg4)) {
-        $getres = getRes($arg1, $arg2, $arg3, $arg4);
-        $resultat = $getres[0]['total'];
-    }
-    elseif (isset($arg3)) {
-        $getres = getRes($arg1, $arg2, $arg3);
-        $resultat = $getres[0]['total'];
-    }
-    elseif (isset($arg2)) {
-        $getres = getRes($arg1, $arg2);
-        $resultat = $getres[0]['total'];
-    }
-    elseif (isset($arg1)){
-        var_dump($arg1);
-        $getres = getRes($arg1);
-        $resultat = $getres[0]['total'];
-    }
-    else{
+    if($_POST['default'] == "default"){
         $getres = getRes();
         $resultat = $getres[0]['total'];
         $caracteristiques_departement = getDepartements();
         $mesdepartements = $caracteristiques_departement[0]['totalDepartement'];
     }
+    else{
+        foreach ($_POST as $key => $value) {
+            if($value !== ""){
+                $arg = "arg".$i;
+                $$arg = $key. "@". $value;
+                $i++;
+            }
+        }
+        
+        // appeler la fonction avec les arguments donnés
+        if(isset($arg5)){
+            $getres = getRes($arg1, $arg2, $arg3, $arg4, $arg5);
+            $resultat = $getres[0]['total'];
+        }
+        elseif (isset($arg4)) {
+            $getres = getRes($arg1, $arg2, $arg3, $arg4);
+            $resultat = $getres[0]['total'];
+        }
+        elseif (isset($arg3)) {
+            $getres = getRes($arg1, $arg2, $arg3);
+            $resultat = $getres[0]['total'];
+        }
+        elseif (isset($arg2)) {
+            $getres = getRes($arg1, $arg2);
+            $resultat = $getres[0]['total'];
+        }
+        elseif (isset($arg1)){
+            var_dump($arg1);
+            $getres = getRes($arg1);
+            $resultat = $getres[0]['total'];
+        }
+    }
 }
+
 else{
     $resultat = "noreq";
 }
 
 $response = [
     "total" => $resultat,
-    "départements" => $mesdepartements,
+    "departements" => $mesdepartements,
 ];
 
 echo $jsonresponse = json_encode($response);
